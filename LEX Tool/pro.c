0a4047dc032e3596a0c31ebb0149798173bf989b
Rishabh Jaiswal <32019969+rsj-rishabh@users.noreply.github.com>
2018-09-27 09:48:03 +0530

    Operations using LEX Tool

---
+++ LEX Tool/pro.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+/*this is main function*/
+int main(){
+  /*this is a comment*/
+  printf("Enter inputs:\n");
+  int a, b, c;
+  scanf("%d%d",&a,&b);
+  /*these are the test cases*/
+  int _d = 5;
+  int e_5 = -4;
+  float 5f = 0.6f;
+  float g8 = -3.8f;
+  c=a+b;
+  b=a+b+c;
+  c=b;
+  /*another comment*/
+  printf("Sum is:  %d\n",&c);
+
+return 0;
+}
